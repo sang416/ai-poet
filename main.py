dfdb95a521e7f891ae534e1cb2658782edfdb290
SANGGYUN LEE <94345744+sang416@users.noreply.github.com>
2024-10-25 21:04:55 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import streamlit as st 
 from langchain_openai import ChatOpenAI
 import streamlit.components.v1 as components
+from streamlit_extras.buy_me_a_coffee import button
 
 st.title("시 작성기")
 components.iframe("https://ads-partners.coupang.com/widgets.html?id=816332&template=carousel&trackingCode=AF7997393", width=680, height=140)
 st.write("---")
+button(username="sang416", floating=True, width=221)
 chat_model = ChatOpenAI()
 
 content = st.text_input("시 작성을 위한 키워드를 입력해주세요.")
